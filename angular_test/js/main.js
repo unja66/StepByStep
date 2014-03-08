b98c1c838b24c16c2bde4d350659ccf6be6dc3c8
UnyongChoi <unyongchoi@gmail.com>
2014-03-09 01:34:24 +0900

    gogo

---
+++ angular_test/js/main.js
@@ -0,0 +1,24 @@
+var app = angular.module('myApp', []);
+
+app.controller('PlayerController', function($scope) {
+	$scope.playing = false;
+	$scope.audio = document.createElement('audio');
+	$scope.audio.src = '/media/sample.mp3';
+	$scope.play = function() {
+		$scope.audio.play();
+		$scope.playing = true;
+	};
+	$scope.stop = function() {
+		$scope.audio.pause();
+		$scope.playing = false;
+	};
+	$scope.audio.addEventListener('ended', function() {
+		$scope.$apply(function() {
+			$scope.stop()
+		});
+	});
+});
+
+app.controller('RelatedController', function($scope){
+});
+
