f4d5f6cbbcc8f71f6f5de1ba62c57ce720187b1a
UnyongChoi <unyongchoi@gmail.com>
2014-03-31 12:10:10 +0900

    changed the title

--- angular_test/js/main.js
+++ angular_test/js/main.js
@@ -1,22 +1,18 @@
 var app = angular.module('myApp', []);
 
+app.controller('MyController', function($scope) {
+	  $scope.person = { name: "Ari Lerner" };
+	    var updateClock = function() {
+			    $scope.clock = new Date();
+				  };
+				    var timer = setInterval(function() {
+						    $scope.$apply(updateClock);
+							  }, 1000);
+					  updateClock();
+});
+
+
 app.controller('PlayerController', function($scope) {
-	$scope.playing = false;
-	$scope.audio = document.createElement('audio');
-	$scope.audio.src = '/media/sample.mp3';
-	$scope.play = function() {
-		$scope.audio.play();
-		$scope.playing = true;
-	};
-	$scope.stop = function() {
-		$scope.audio.pause();
-		$scope.playing = false;
-	};
-	$scope.audio.addEventListener('ended', function() {
-		$scope.$apply(function() {
-			$scope.stop()
-		});
-	});
 });
 
 app.controller('RelatedController', function($scope){
